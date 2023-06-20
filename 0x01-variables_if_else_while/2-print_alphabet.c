a6d219781f114bf16a89cec0aa279908cb63048c
Shalaby000 <shalaby.trabajo@gmail.com>
2023-06-20 04:21:09 +0300

    s

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,19 +1,18 @@
 #include <stdio.h>
 
 /**
- * main - prints the alphabet in lowercase,
- * followed by a new line
- * putc is including unsigned char
+ * main - prints the alphabet in lowercase.
+ * 
  * Return:  0 = (Success)
  */
 int main(void)
 {
-	char ch;
+	char letter;
 
-	for (ch = 'a'; ch <= 'z'; ch++)
-	{
-	    putchar(ch);
-	}
+	for (letter = 'a'; letter <= 'z'; letter++)
+		putchar(letter);
+
 	putchar('\n');
+
 	return (0);
 }
