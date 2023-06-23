bbc115cb9a7430b001b7d759e20d58274bebcd75
Shalaby000 <shalaby.trabajo@gmail.com>
2023-06-23 04:46:33 +0300

    s

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -6,16 +6,20 @@
  *
  * Return: void
  */
-voidd print_line(int n)
+void print_line(int n)
 {
 	int lnChr;
 
 	if (n <= 0)
+	{
 		_putchar('\n');
+	}
 	else
 	{
 		for (lnChr = 1; lnChr <= n; lnChr++)
+		{
 			_putchar('_');
+		}
 		_putchar('\n');
 	}
 }
