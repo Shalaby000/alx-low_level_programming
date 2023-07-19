58672b236db2d51a5f59f53af3fae38edbb0e7a0
Shalaby000 <shalaby.trabajo@gmail.com>
2023-07-19 05:43:50 +0300

    s

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * main - check the code
+ * Description: just a long dog struct in a big kitty world
+ * Return: Always 0.
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
+
