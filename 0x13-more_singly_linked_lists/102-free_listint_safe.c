e24d0a0dc719df5b0439e2f013620587dbfec294
Shalaby000 <shalaby.trabajo@gmail.com>
2023-08-01 04:34:58 +0300

    s

---
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -0,0 +1,72 @@
+#include "lists.h"
+
+/**
+ * free_listp2 - frees a linked list
+ * @head: head of a list
+ *
+ * Return: void
+ */
+void free_listp2(listp_t **head)
+{
+	listp_t *temp;
+	listp_t *nid;
+
+	if (head != NULL)
+	{
+		nid = *head;
+		while ((temp = nid) != NULL)
+		{
+			nid = nid->next;
+			free(temp);
+		}
+		*head = NULL;
+	}
+}
+
+/**
+ * free_listint_safe - frees a linked list.
+ * @h: head of a list.
+ *
+ * Return: size of the list that was freed.
+ */
+size_t free_listint_safe(listint_t **h)
+{
+	size_t nnodes = 0;
+	listp_t *hptr, *new, *add;
+	listint_t *nid;
+
+	hptr = NULL;
+	while (*h != NULL)
+	{
+		new = malloc(sizeof(listp_t));
+
+		if (new == NULL)
+			exit(98);
+
+		new->p = (void *)*h;
+		new->next = hptr;
+		hptr = new;
+
+		add = hptr;
+
+		while (add->next != NULL)
+		{
+			add = add->next;
+			if (*h == add->p)
+			{
+				*h = NULL;
+				free_listp2(&hptr);
+				return (nnodes);
+			}
+		}
+
+		nid = *h;
+		*h = (*h)->next;
+		free(nid);
+		nnodes++;
+	}
+
+	*h = NULL;
+	free_listp2(&hptr);
+	return (nnodes);
+}
