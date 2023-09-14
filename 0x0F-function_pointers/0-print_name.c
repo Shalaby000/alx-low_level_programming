fe3c13316fae13b96f6104696e656f26b004ce88
floki <shalaby.trabajo@gmail.com>
2023-09-14 04:49:25 +0200

    s

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,16 +1,15 @@
 #include "function_pointers.h"
 
 /**
- * print_name - A function that prints a name
- * @name: The name to be printed
- * @f: Pointer to a function
+ * print_name - function that prints a name.
+ * @name: pointer to name.
+ * @f: pointer to function.
  *
- * Return: void
+ * Return: No return.
  */
+
 void print_name(char *name, void (*f)(char *))
 {
 	if (name && f)
-    {
-	    f(name);
-    }
+		f(name);
 }
