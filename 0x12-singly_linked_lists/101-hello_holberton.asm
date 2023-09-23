c288ff53f6c5c72e9d9abd836053cd8e3cc11df4
floki <shalaby.trabajo@gmail.com>
2023-09-23 04:19:35 +0200

    s

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,24 +1,16 @@
-section .data
-    hello db 'Hello, Holberton', 0Ah ; Null-terminated string
-
-section .text
-    global _start
-
 ; ----------------------------------------
 ; Function: _start
 ; Entry point of the program.
 ; Prints "Hello, Holberton" followed by a new line.
 ; Exits the program.
 ; ----------------------------------------
-_start:
-    ; Write the string to stdout
-    mov rax, 1                  ; System call number for write (1)
-    mov rdi, 1                  ; File descriptor (1 = stdout)
-    mov rsi, hello              ; Address of the string
-    mov rdx, 16                 ; Length of the string
-    syscall
 
-    ; Exit the program
-    mov eax, 60                 ; System call number for exit (60)
-    xor edi, edi                ; Exit status (0)
-    syscall
+          global    main
+          extern    printf
+main:
+	  mov   edi, format
+	  xor   eax, eax
+	  call  printf
+	  mov 	eax, 0
+	  ret
+format: db `Hello, Holberton\n`,0
