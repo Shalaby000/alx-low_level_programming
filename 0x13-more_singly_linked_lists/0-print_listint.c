d342cad74a8865dc575c1f043075b0e3f13b7964
floki <shalaby.trabajo@gmail.com>
2023-09-26 10:05:17 +0200

    s

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,18 +1,23 @@
 #include "lists.h"
+#include <stdio.h>
 
 /**
- * print_listint - prints all the elements of a linked list
+ * print_listint - Function that prints all elements of a list
  * @h: head of the list
  *
- * Return: the number of nodes
+ * Return: the number of nodes of the list
  */
+
 size_t print_listint(const listint_t *h)
 {
-	size_t i = 0;
+	size_t nodes = 0;
 
 	while (h)
+	{
+		nodes++;
 		printf("%d\n", h->n);
 		h = h->next;
-		i++;
-	return (i);
+	}
+
+	return (nodes);
 }
