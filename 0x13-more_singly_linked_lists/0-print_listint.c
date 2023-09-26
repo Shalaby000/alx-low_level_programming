5a252841d643ca31f4818c1489e1bccd5d1e0bff
floki <shalaby.trabajo@gmail.com>
2023-09-26 11:17:27 +0200

    s

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -2,12 +2,11 @@
 #include <stdio.h>
 
 /**
- * print_listint - Function that prints all elements of a list
- * @h: head of the list
- *
- * Return: the number of nodes of the list
- */
-
+* print_listint - Function that prints all elements of a list
+* @h: head of the list
+*
+* Return: the number of nodes of the list
+*/
 size_t print_listint(const listint_t *h)
 {
 	size_t nodes = 0;
