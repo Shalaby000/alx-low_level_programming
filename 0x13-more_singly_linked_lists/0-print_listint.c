f2ef01435a2321d170aaafbb340dc15c57d0f6c8
floki <shalaby.trabajo@gmail.com>
2023-09-26 10:07:18 +0200

    s

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -7,17 +7,16 @@
  *
  * Return: the number of nodes of the list
  */
-
 size_t print_listint(const listint_t *h)
 {
-	size_t nodes = 0;
+	size_t np = 0;
 
 	while (h)
 	{
-		nodes++;
+		np++;
 		printf("%d\n", h->n);
 		h = h->next;
 	}
 
-	return (nodes);
+	return (np);
 }
